61d153415d7e93e241b3267879218c945ca759c8
bboythian <bboythian@gmail.com>
2024-05-23 10:48:23 -0500

    Conexion con telefonos

--- webLogin.js
+++ webLogin.js
@@ -71,4 +71,77 @@ app.get('/get-reportes', (req, res) => {
         });
 });
 
+// Ruta para agregar un nuevo reporte desde la aplicación móvil Flutter
+app.post('/enviar-datos', (req, res) => {
+    const { cedula, fecha, mayorConsumo, usageData } = req.body;
+
+    if (usageData) {
+        // Parsear los datos enviados desde el móvil
+        const parsedDataToSend = JSON.parse(usageData);
+
+        // Construir un objeto Reporte con los datos recibidos
+        const nuevoReporte = new Reporte({
+            cedula,
+            fecha,
+            mayorConsumo,
+        });
+        // Definir una función para obtener los campos packageName y tiempoUso
+        const getFieldData = (index, field) => {
+            return parsedDataToSend[index] ? parsedDataToSend[index][field] : '-';
+        };
+        // Iterar sobre un número máximo de elementos y asignar los valores correspondientes
+        const maxElements = 5;
+        for (let i = 0; i < maxElements; i++) {
+            nuevoReporte[`packageName${i + 1}`] = getFieldData(i, 'packageName');
+            nuevoReporte[`tiempoUso${i + 1}`] = getFieldData(i, 'totalTimeInForeground');
+        }
+
+        nuevoReporte.save()
+            .then(reporte => {
+                res.status(200).send('Datos recibidos correctamente'); // Enviar una respuesta exitosa
+            })
+            .catch(err => {
+                console.error('Error al agregar nuevo reporte:', err);
+                res.status(500).send('Error al procesar los datos'); // Enviar una respuesta de error en caso de problemas
+            });
+    } else {
+        // Si no se recibieron datos de uso de aplicaciones, enviar una lista vacía
+        const nuevoReporte = new Reporte({
+            cedula,
+            fecha,
+            mayorConsumo,
+            packageName1: '-',
+            packageName2: '-',
+            packageName3: '-',
+            packageName4: '-',
+            packageName5: '-',
+            tiempoUso1: 0,
+            tiempoUso2: 0,
+            tiempoUso3: 0,
+            tiempoUso4: 0,
+            tiempoUso5: 0,
+        });
+        nuevoReporte.save()
+            .then(reporte => {
+                res.status(200).send('Datos recibidos correctamente'); // Enviar una respuesta exitosa
+            })
+            .catch(err => {
+                console.error('Error al agregar nuevo reporte:', err);
+                res.status(500).send('Error al procesar los datos'); // Enviar una respuesta de error en caso de problemas
+            });
+    }
+});
+// Ruta para eliminar un usuario
+app.post('/delete-reporte', (req, res) => {
+    Reporte.findOneAndDelete({ _id: req.body.reporteId })
+        .then(() => {
+            res.redirect('/main'); // Redirigir de vuelta a la página principal después de eliminar usuario
+        })
+        .catch(err => {
+            console.error('Error al eliminar usuario:', err);
+            res.redirect('/'); // Redirigir de vuelta a la página principal en caso de error
+        });
+});
+
+
 module.exports = app;
